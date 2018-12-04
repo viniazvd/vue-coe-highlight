282addac4697a8a3e5c05dc212f44268a45b4f81
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-12-04 19:49:27 -0200

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+
+const VueCoeHighLight = {
+  functional: true,
+
+  props: { code: String },
+
+  render (h, { props, children }) {
+    const code = children && !!children.length && children[0].text
+
+    if (!props.coce && !code) {
+      console.warn('code is required!')
+      return false
+    }
+
+    return h('pre',
+      [
+        h('code', {
+          class: `language-markup`,
+          domProps: {
+            innerHTML: Prism.highlight(props.code || code, Prism.languages['markup'])
+          }
+        })
+      ]
+    )
+  }
+}
+
+export default VueCoeHighLight
+
+export const VueCoeImage = {
+  install (Vue) {
+    Vue.component('VueCoeHighLight', VueCoeHighLight)
+  }
+}
